4bb7250da5bb391821b06d446f8ce3a9f337c49c
Loïc Nogues <nogues.loic@gmail.com>
2017-07-20 14:44:57 +0200

    Reversed path

--- js/nunjucks_extensions.js
+++ js/nunjucks_extensions.js
@@ -4,8 +4,8 @@ const path = require('path')
   , nunjucks = require('nunjucks');
 
 const templatePath = [
-    path.resolve(__dirname, '../node_modules/liveblog-default-theme/templates'),
-    path.resolve(__dirname, '../templates')
+    path.resolve(__dirname, '../templates'),
+    path.resolve(__dirname, '../node_modules/liveblog-default-theme/templates')
   ]
   , nunjucksLoader = new nunjucks.FileSystemLoader(templatePath)
   , nunjucksEnv = new nunjucks.Environment(nunjucksLoader);
